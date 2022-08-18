678a14c95a688dd3fcc591c4f9a763f12cc06c5e
Kakha <kakh20@freeuni.edu.ge>
2022-08-19 00:28:18 +0400

    Creating RegisterValidator Class, as a mock class for now

---
+++ untitled/src/main/java/Validator/RegisterValidator.java
@@ -0,0 +1,41 @@
+package Validator;
+
+import Errors.AppError;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegisterValidator implements Validator{
+
+    private final String username;
+    private final String firstName;
+    private final String lastName;
+    private final String password;
+    private final Connection connection;
+
+    private List<AppError> errors;
+
+    public RegisterValidator(String username, String firstName, String lastName, String password, Connection dataBaseConn) {
+        this.username = username;
+        
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+        this.password = password;
+        this.connection = dataBaseConn;
+
+        this.errors = new ArrayList<>();
+    }
+
+    @Override
+    public boolean validate() throws SQLException {
+        return false; // TODO NEEDS TO BE IMPLEMENTED
+    }
+
+    @Override
+    public List<AppError> getErrors() {
+        return errors;
+    }
+}
