3e1577e4dfaf4c49d69b2eb86d95caad4da463a7
Kakha <kakh20@freeuni.edu.ge>
2022-08-19 01:02:03 +0400

    Mark to think about other database structure (NOT LOCAL ONE)

--- untitled/src/main/java/Quiz/Model/DatabaseConnector.java
+++ untitled/src/main/java/Quiz/Model/DatabaseConnector.java
@@ -10,6 +10,7 @@ import java.sql.*;
 
 public class DatabaseConnector {
 
+    // TODO NEEDS REWORK
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         String dbName = "quiz_db";
         String sqlUsername = "root";
