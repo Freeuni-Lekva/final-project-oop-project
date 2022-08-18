2e6c9fccf5f76b61d5acec619b900011ed91ad6f
Kakha <kakh20@freeuni.edu.ge>
2022-08-18 23:05:31 +0400

    Changing password for Personal use

--- untitled/src/main/java/Model/DatabaseConnector.java
+++ untitled/src/main/java/Model/DatabaseConnector.java
@@ -13,7 +13,7 @@ public class DatabaseConnector {
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         String dbName = "quiz_db";
         String sqlUsername = "root";
-        String sqlPassword = "rootroot";
+        String sqlPassword = "123456789OOp!";
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection("jdbc:mysql://localhost:3306/" + dbName, sqlUsername, sqlPassword);}
 
