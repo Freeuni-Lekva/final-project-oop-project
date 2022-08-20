dd825bc5f2cb8db87a4597567f295ced9c5c1118
Kakha <kakh20@freeuni.edu.ge>
2022-08-20 23:46:13 +0400

    Add PasswordValidator

---
+++ Quizzetta/src/main/java/com/quizzetta/Validator/PasswordValidator.java
@@ -0,0 +1,60 @@
+package com.quizzetta.Validator;
+
+import com.quizzetta.Errors.AppError;
+import com.quizzetta.Errors.EmptyInputError;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class PasswordValidator implements Validator {
+    public static final int PASSWORD_MIN_LENGTH = 8;
+
+    public static final String PASSWORD_LENGTH_ERROR = "Password should contain at least 8 characters";
+
+
+    public static final String PASSWORD_UPPERCASE_ERROR = "Password should contain at least 1 upper case character";
+
+    public static final String PASSWORD_LOWERCASE_ERROR = "Password should contain at least 1 lower case character";
+
+    public static final String PASSWORD_DIGIT_ERROR = "Password should contain at least 1 digit";
+
+
+    private String password;
+    private  List<AppError> errors;
+    public PasswordValidator(String password) {
+        this.password = password;
+        errors = new ArrayList<>();
+    }
+
+    @Override
+    public boolean validate() throws SQLException {
+        if (password.length() < PASSWORD_MIN_LENGTH) {
+            errors.add(new EmptyInputError("password", PASSWORD_LENGTH_ERROR));
+        }
+
+        Pattern upperCaseValuesPattern = Pattern.compile("[A-Z]");
+        Pattern lowerCaseValuesPattern = Pattern.compile("[a-z]");
+        Pattern digitValuesPattern = Pattern.compile("[0-9]");
+
+        if (!upperCaseValuesPattern.matcher(password).find()) {
+            errors.add(new EmptyInputError("password", PASSWORD_UPPERCASE_ERROR));
+        }
+
+        if (!lowerCaseValuesPattern.matcher(password).find()) {
+            errors.add(new EmptyInputError("password", PASSWORD_LOWERCASE_ERROR));
+        }
+
+        if (!digitValuesPattern.matcher(password).find()) {
+            errors.add(new EmptyInputError("password", PASSWORD_DIGIT_ERROR));
+        }
+
+        return errors.size() == 0;
+    }
+
+    @Override
+    public List<AppError> getErrors() {
+        return errors;
+    }
+}
