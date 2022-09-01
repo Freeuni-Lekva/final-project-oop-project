49656960861107313d059879563352451e48c5d2
Erybor <eryborml@gmail.com>
2022-09-01 15:16:40 +0400

    Updated errors, Error interface needs rework, leaving it for now

--- Quizzetta/src/main/java/com/quizzetta/Errors/EmptyInputError.java
+++ Quizzetta/src/main/java/com/quizzetta/Errors/EmptyInputError.java
@@ -4,12 +4,14 @@ public class EmptyInputError implements AppError {
 
     private final String errorMessage;
     private final String inputFieldName;
+    private final String errorName;
 
-    public EmptyInputError(String inputFieldName ,String errorMessage) {
+    public EmptyInputError(String inputFieldName, String errorMessage) {
+        this.errorName = "EmptyInputError";
         this.inputFieldName = inputFieldName;
         this.errorMessage = errorMessage;
     }
-    
+
     @Override
     public String getErrorMessage() {
         return errorMessage;
--- Quizzetta/src/main/java/com/quizzetta/Errors/UserNotFoundError.java
+++ Quizzetta/src/main/java/com/quizzetta/Errors/UserNotFoundError.java
@@ -1,2 +1,17 @@
-package com.quizzetta.Errors;public class UserNotFoundError {
+package com.quizzetta.Errors;
+
+public class UserNotFoundError implements AppError {
+
+    private final String errorMessage;
+    private final String inputFieldName;
+
+    public UserNotFoundError(String inputFieldName, String errorMessage) {
+        this.inputFieldName = inputFieldName;
+        this.errorMessage = errorMessage;
+    }
+
+    @Override
+    public String getErrorMessage() {
+        return errorMessage;
+    }
 }
