f8603b277607fb8761029d9aecf5a77160bd7de6
Kakha <kakh20@freeuni.edu.ge>
2022-09-10 00:45:51 +0400

    Temp commit for checkout

--- Quizzetta/src/main/java/com/quizzetta/DAOs/PictureResponseQuestionDAO.java
+++ Quizzetta/src/main/java/com/quizzetta/DAOs/PictureResponseQuestionDAO.java
@@ -14,70 +14,70 @@ public class PictureResponseQuestionDAO implements QuestionDAO, AnswerDAO {
 
     private final Connection myConn;
 
-    public PictureResponseQuestionDAO (Connection conn) {
+    public PictureResponseQuestionDAO(Connection conn) {
         myConn = conn;
     }
 
     @Override
-    public void addAnswer(ArrayList<Answer> answers, long questionId) {
-        PreparedStatement stm;
-        try {
-            for (Answer ans : answers) {
-                stm = myConn.prepareStatement("INSERT INTO pictureResponseAnswers " +
-                                "(answer_text, question_id) VALUES (?, ?)",
-                        PreparedStatement.RETURN_GENERATED_KEYS);
-                stm.setString(1, ans.getText());
-                stm.setLong(2, questionId);
-                stm.execute();
-                ResultSet res = stm.getGeneratedKeys();
-                res.next();
-                long answerId = res.getLong(1);
-                ans.setId(answerId);
-                ans.setIsCorrect(true);
-            }
-        } catch(SQLException e) {
-            throw new RuntimeException();
+    public void addAnswer(ArrayList<Answer> answers, long questionId) throws SQLException {
+        for (Answer ans : answers) {
+            PreparedStatement stm = myConn.prepareStatement("INSERT INTO pictureResponseAnswers " +
+                            "(answer_text, question_id) VALUES (?, ?)",
+                    PreparedStatement.RETURN_GENERATED_KEYS);
+            stm.setString(1, ans.getText());
+            stm.setLong(2, questionId);
+            stm.execute();
+            ResultSet res = stm.getGeneratedKeys();
+            res.next();
+            long answerId = res.getLong(1);
+            ans.setId(answerId);
+            ans.setIsCorrect(true);
         }
     }
 
-    @Override
-    public List<Answer> getAnswer(long questionId) {
-        PreparedStatement stm;
-
+    public void addAnswer(Answer answer, long questionId) {
         try {
-            stm = myConn.prepareStatement("SELECT * FROM pictureResponseAnswers WHERE question_id = ?");
-            stm.setLong(1, questionId);
-            ResultSet res = stm.executeQuery();
-            List<Answer> lst = new ArrayList<>();
-            while (res.next()) {
-                lst.add(new Answer(res.getLong("id"), res.getString("answer_text"),
-                        res.getLong("question_id"), true));
-            }
-            return lst;
-        } catch(SQLException e) {
-            throw new RuntimeException();
-        }
-    }
-
-    @Override
-    public void removeAnswer(long answerId) {
-        PreparedStatement stm;
-
-        try {
-            stm = myConn.prepareStatement("DELETE FROM pictureResponseAnswers WHERE id = ?");
-            stm.setLong(1, answerId);
-            stm.executeUpdate();
+            PreparedStatement stm = myConn.prepareStatement("INSERT INTO pictureResponseAnswers " +
+                            "(answer_text, question_id) VALUES (?, ?)",
+                    PreparedStatement.RETURN_GENERATED_KEYS);
+            stm.setString(1, answer.getText());
+            stm.setLong(2, questionId);
+            stm.execute();
+            ResultSet res = stm.getGeneratedKeys();
+            res.next();
+            long answerId = res.getLong(1);
+            answer.setId(answerId);
+            answer.setIsCorrect(true);
         } catch (SQLException e) {
-            throw new RuntimeException();
+            e.printStackTrace();
         }
     }
 
-    @Override
-    public void addQuestion(Question question, long quizId)  {
-        PreparedStatement stm;
 
+    @Override
+    public List<Answer> getAnswer(long questionId) throws SQLException {
+        PreparedStatement stm = myConn.prepareStatement("SELECT * FROM pictureResponseAnswers WHERE question_id = ?");
+        stm.setLong(1, questionId);
+        ResultSet res = stm.executeQuery();
+        List<Answer> lst = new ArrayList<>();
+        while (res.next()) {
+            lst.add(new Answer(res.getLong("id"), res.getString("answer_text"),
+                    res.getLong("question_id"), true));
+        }
+        return lst;
+    }
+
+    @Override
+    public void removeAnswer(long answerId) throws SQLException {
+        PreparedStatement stm = myConn.prepareStatement("DELETE FROM pictureResponseAnswers WHERE id = ?");
+        stm.setLong(1, answerId);
+        stm.executeUpdate();
+    }
+
+    @Override
+    public void addQuestion(Question question, long quizId) {
         try {
-            stm = myConn.prepareStatement("INSERT INTO pictureResponseQuestions " +
+            PreparedStatement stm = myConn.prepareStatement("INSERT INTO pictureResponseQuestions " +
                             "(question_text, url, quiz_id) VALUES (?, ?, ?)",
                     PreparedStatement.RETURN_GENERATED_KEYS);
             stm.setString(1, question.getText());
@@ -89,54 +89,36 @@ public class PictureResponseQuestionDAO implements QuestionDAO, AnswerDAO {
             long questionId = res.getLong(1);
             question.setId(questionId);
         } catch (SQLException e) {
-            throw new RuntimeException();
+            e.printStackTrace();
         }
     }
 
     @Override
-    public Question getQuestion(long questionId) {
-        PreparedStatement stm;
-
-        try {
-            stm = myConn.prepareStatement("SELECT * FROM pictureResponseQuestions WHERE id = ?");
-            stm.setLong(1, questionId);
-            ResultSet res = stm.executeQuery();
-            res.next();
-            return new Question(res.getLong("id"), res.getString("question_text"),
-                    res.getLong("quiz_id"), res.getString("url"), 1);
-        }catch (SQLException e) {
-            throw new RuntimeException();
-        }
+    public Question getQuestion(long questionId) throws SQLException {
+        PreparedStatement stm = myConn.prepareStatement("SELECT * FROM pictureResponseQuestions WHERE id = ?");
+        stm.setLong(1, questionId);
+        ResultSet res = stm.executeQuery();
+        res.next();
+        return new Question(res.getLong("id"), res.getString("question_text"),
+                res.getLong("quiz_id"), res.getString("url"), 1);
     }
 
     @Override
-    public List<Question> getAllQuestions(long quizId) {
-        PreparedStatement stm;
-
-        try {
-            stm = myConn.prepareStatement("SELECT * FROM pictureResponseQuestions WHERE quiz_id = ?");
-            stm.setLong(1, quizId);
-            List<Question> questionList = new ArrayList<>();
-            ResultSet res = stm.executeQuery();
-            while (res.next()) {
-                questionList.add(getQuestion(res.getLong("id")));
-            }
-            return questionList;
-        } catch (SQLException e) {
-            throw new RuntimeException();
+    public List<Question> getAllQuestions(long quizId) throws SQLException {
+        PreparedStatement stm = myConn.prepareStatement("SELECT * FROM pictureResponseQuestions WHERE quiz_id = ?");
+        stm.setLong(1, quizId);
+        List<Question> questionList = new ArrayList<>();
+        ResultSet res = stm.executeQuery();
+        while (res.next()) {
+            questionList.add(getQuestion(res.getLong("id")));
         }
+        return questionList;
     }
 
     @Override
-    public void removeQuestion(long questionId) {
-        PreparedStatement stm;
-
-        try {
-            stm = myConn.prepareStatement("DELETE FROM pictureResponseQuestions WHERE id = ?");
-            stm.setLong(1, questionId);
-            stm.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException();
-        }
+    public void removeQuestion(long questionId) throws SQLException {
+        PreparedStatement stm = myConn.prepareStatement("DELETE FROM pictureResponseQuestions WHERE id = ?");
+        stm.setLong(1, questionId);
+        stm.executeUpdate();
     }
 }
