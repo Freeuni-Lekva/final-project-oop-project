2d352596c3dc9a2a98fb9919c9354706be115361
Kakha <kakh20@freeuni.edu.ge>
2022-09-10 15:53:30 +0400

    Friend Request Dao Correction and sevlets stubs

---
+++ Quizzetta/src/main/java/com/quizzetta/Sevices/FriendshipControl/AddFriendServlet.java
@@ -0,0 +1,34 @@
+package com.quizzetta.Sevices.FriendshipControl;
+
+
+import com.quizzetta.DAOs.FriendRequestDAO;
+import com.quizzetta.DAOs.QuizDAO;
+import com.quizzetta.Model.FriendRequest;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("/SendFriendRequest")
+public class AddFriendServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int sendID = Integer.parseInt(req.getParameter("sender"));
+        int receiverID = Integer.parseInt(req.getParameter("receiver"));
+
+        FriendRequestDAO friendRequestDAO = (FriendRequestDAO) req.getServletContext().getAttribute("FriendRequestDAO");
+        FriendRequest friendRequest = new FriendRequest(sendID, receiverID);
+
+        friendRequestDAO.addFriendRequest(friendRequest);
+
+        resp.sendRedirect(""); // TODO TO USER
+    }
+}
